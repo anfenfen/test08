cb0602a0ccb09200d171faec1020e77b198a992f
anfenfen <15809287557@sina.cn>
2016-06-07 21:11:14 +0800

    test08

---
+++ src/collect_all_even.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function collect_all_even(collection) {
+   var arry=new Array();
+  var k=0;
+  for(var i=0;i<collection.length;i++)
+  {
+  	if(collection[i]%2==0)
+  	{
+  		arry[k]=collection[i];
+  		k++;
+  	}
+  }
+  return arry;
+}
+
+module.exports = collect_all_even;
